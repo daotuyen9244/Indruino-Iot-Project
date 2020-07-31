b3702d34ec250c07c6d5c626f4438bc3069c7b9e
unknown <xzhoangphuc97@gmail.com>
2020-07-31 19:56:22 +0700

    update week 3

---
+++ FW/Phuc/websever/websever_2nd.ino/websever_2nd.ino.ino
@@ -0,0 +1,259 @@
+
+// Load libraries
+#include "heltec.h"
+#include "WiFi.h"
+#include "images.h"
+
+bool wfconnect;
+String LocalIP;
+
+void logo(){
+  Heltec.display -> init();
+  Heltec.display -> clear();
+  Heltec.display -> drawXbm(0,5,Indruino_Logo_width,Indruino_Logo_height,(const unsigned char *)Indruino_Logo_bits);
+  Heltec.display -> display();
+}
+// Replace with your network credentials
+const char* ssid = "Indruino_Student";    //WiFi.begin("Tom","xz001122");   WiFi.begin("Indruino_Student","Indruino2019"); WiFi.begin("bybeen","00112233")
+const char* password = "Indruino2019";
+
+const char* ssid_1 = "bybeen";    
+const char* password_1 = "00112233";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output26State = "off";
+String output27State = "off";
+
+// Assign output variables to GPIO pins
+const int output26 = 26;
+const int output27 = 27;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+byte callwifi()
+{
+  byte count=0;
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED && count < 20)
+  {
+    count ++;
+    if(count >= 10 && count <= 20)
+      WiFi.begin(ssid_1, password_1);
+    delay(180);
+    Heltec.display -> drawString(0, 0, "Connecting...");
+    Heltec.display -> display();
+  }
+  return count;
+}
+void wifiok(byte count)
+{
+  Heltec.display -> clear();
+    Heltec.display -> drawString(0, 0, "Connecting...OK.");
+    LocalIP = String() + WiFi.localIP()[0] + "." + WiFi.localIP()[1] + "." + WiFi.localIP()[2] + "." +WiFi.localIP()[3];
+    Heltec.display -> drawString(0, 10, "ConnectIP:");
+    Heltec.display -> drawString(50, 10,LocalIP);
+    Heltec.display -> display();
+    if(count < 10) {
+    Heltec.display -> drawString(0, 20, "SSID:");
+    Heltec.display -> drawString(28, 20,ssid);
+    Heltec.display -> display(); }
+      else if(count >= 10 && count <= 20)
+        {Heltec.display -> drawString(0, 20, "SSID:");
+        Heltec.display -> drawString(28, 20,ssid_1);
+        Heltec.display -> display(); }
+    wfconnect=true;
+    delay(500);
+    server.begin();
+}
+void setup() {
+  logo();
+  delay(1000);
+  Heltec.display -> clear();
+  Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(output26, OUTPUT);
+  pinMode(output27, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output26, LOW);
+  digitalWrite(output27, LOW);
+
+  // Connect to Wi-Fi network with SSID and password
+  byte count=callwifi();
+  
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    Heltec.display -> clear();
+    Heltec.display -> drawString(0, 0, "Connecting...Failed");
+    Heltec.display -> display();
+    wfconnect=false;
+    Heltec.display -> drawString(0, 10, "Try to connect...");
+    Heltec.display -> display();
+    count=callwifi();
+  }
+      wfconnect=true;
+  if(wfconnect==true)
+  {
+  wifiok(count);
+  Heltec.display -> drawString(0, 30, "WIFI Setup done");
+  Heltec.display -> display();
+  delay(500);
+  }
+}
+
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /26/on") >= 0) {
+              Heltec.display -> clear();
+              Heltec.display -> drawString(0, 0, "ConnectIP:");
+              Heltec.display -> drawString(50, 0,LocalIP);
+              Heltec.display -> drawString(0, 10,"GPIO 26 ON");
+              Heltec.display -> display();
+                    if(output27State=="on")
+                          {
+                            Heltec.display -> drawString(0, 20,"GPIO 27 ON");
+                            Heltec.display -> display();}
+                            else
+                            {
+                            Heltec.display -> drawString(0, 20,"GPIO 27 OFF");
+                            Heltec.display -> display();}
+                            
+              output26State = "on";
+              digitalWrite(output26, HIGH);
+            } else if (header.indexOf("GET /26/off") >= 0) {
+              Heltec.display -> clear();
+               Heltec.display -> drawString(0, 0, "ConnectIP:");
+              Heltec.display -> drawString(50, 0,LocalIP);
+              Heltec.display -> drawString(0, 10,"GPIO 26 OFF");
+              Heltec.display -> display();
+                          if(output27State=="on")
+                          {
+                            Heltec.display -> drawString(0, 20,"GPIO 27 ON");
+                            Heltec.display -> display();}
+                            else
+                            {
+                            Heltec.display -> drawString(0, 20,"GPIO 27 OFF");
+                            Heltec.display -> display();}
+              output26State = "off";
+              digitalWrite(output26, LOW);
+            } else if (header.indexOf("GET /27/on") >= 0) {
+         
+              Heltec.display -> clear();
+               Heltec.display -> drawString(0, 0, "ConnectIP:");
+              Heltec.display -> drawString(50, 0,LocalIP);
+              Heltec.display -> drawString(0, 20,"GPIO 27 ON");
+              Heltec.display -> display();
+              if(output26State=="on")
+                          {
+                            Heltec.display -> drawString(0, 10,"GPIO 26 ON");
+                            Heltec.display -> display();}
+                            else
+                            {
+                            Heltec.display -> drawString(0, 10,"GPIO 26 OFF");
+                            Heltec.display -> display();}
+              output27State = "on";
+              digitalWrite(output27, HIGH);
+            } else if (header.indexOf("GET /27/off") >= 0) {
+              
+              Heltec.display -> clear();
+              Heltec.display -> drawString(0, 0, "ConnectIP:");
+              Heltec.display -> drawString(50, 0,LocalIP);
+              Heltec.display -> drawString(0, 20,"GPIO 27 OFF");
+              Heltec.display -> display();
+              if(output26State=="on")
+                          {
+                            Heltec.display -> drawString(0, 10,"GPIO 26 ON");
+                            Heltec.display -> display();}
+                            else
+                            {
+                            Heltec.display -> drawString(0, 10,"GPIO 26 OFF");
+                            Heltec.display -> display();}
+              output27State = "off";
+              digitalWrite(output27, LOW);
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 26  
+            client.println("<p>GPIO 26 - State " + output26State + "</p>");
+            // If the output26State is off, it displays the ON button       
+            if (output26State=="off") {
+              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 27  
+            client.println("<p>GPIO 27 - State " + output27State + "</p>");
+            // If the output27State is off, it displays the ON button       
+            if (output27State=="off") {
+              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    
+  }
+}
