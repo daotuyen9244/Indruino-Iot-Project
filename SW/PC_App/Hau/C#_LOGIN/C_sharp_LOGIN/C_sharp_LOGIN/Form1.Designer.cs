2cadc9ff794d76759e18ee8f7a46b0f5a41e60d6
truonghau8 <truonghau8@gmail.com>
2020-08-10 01:47:28 +0700

    Update PC_App and my TODO

---
+++ SW/PC_App/Hau/C#_LOGIN/C_sharp_LOGIN/C_sharp_LOGIN/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿using System.Drawing;
+
+namespace C_sharp_LOGIN
+{
+    partial class F_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_exit = new FontAwesome.Sharp.IconButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txt_UserName = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.btn_login = new FontAwesome.Sharp.IconButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.txt_PassWord = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.lb_infor = new System.Windows.Forms.Label();
+            this.tmr_login_flase = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(74)))), ((int)(((byte)(181)))));
+            this.panel1.Controls.Add(this.btn_exit);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(365, 121);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.AutoSize = true;
+            this.btn_exit.FlatAppearance.BorderSize = 0;
+            this.btn_exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btn_exit.IconChar = FontAwesome.Sharp.IconChar.TimesCircle;
+            this.btn_exit.IconColor = System.Drawing.Color.Black;
+            this.btn_exit.IconSize = 25;
+            this.btn_exit.Location = new System.Drawing.Point(315, -9);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Rotation = 0D;
+            this.btn_exit.Size = new System.Drawing.Size(41, 38);
+            this.btn_exit.TabIndex = 8;
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::C_sharp_LOGIN.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(365, 117);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 10;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.pictureBox1;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 78);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LOGIN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 121);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(365, 78);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.txt_UserName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_UserName.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_UserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txt_UserName.HintForeColor = System.Drawing.Color.Empty;
+            this.txt_UserName.HintText = "username";
+            this.txt_UserName.isPassword = false;
+            this.txt_UserName.LineFocusedColor = System.Drawing.Color.Blue;
+            this.txt_UserName.LineIdleColor = System.Drawing.Color.Gray;
+            this.txt_UserName.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.txt_UserName.LineThickness = 2;
+            this.txt_UserName.Location = new System.Drawing.Point(48, 17);
+            this.txt_UserName.Margin = new System.Windows.Forms.Padding(5);
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.Size = new System.Drawing.Size(283, 37);
+            this.txt_UserName.TabIndex = 4;
+            this.txt_UserName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_UserName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_UserName_KeyPress);
+            // 
+            // btn_login
+            // 
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btn_login.Font = new System.Drawing.Font("Californian FB", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.IconChar = FontAwesome.Sharp.IconChar.Users;
+            this.btn_login.IconColor = System.Drawing.Color.Black;
+            this.btn_login.IconSize = 20;
+            this.btn_login.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_login.Location = new System.Drawing.Point(144, 381);
+            this.btn_login.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Rotation = 0D;
+            this.btn_login.Size = new System.Drawing.Size(90, 39);
+            this.btn_login.TabIndex = 5;
+            this.btn_login.Text = "LOGIN";
+            this.btn_login.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.iconPictureBox2);
+            this.panel2.Controls.Add(this.iconPictureBox1);
+            this.panel2.Controls.Add(this.txt_PassWord);
+            this.panel2.Controls.Add(this.txt_UserName);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 199);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(365, 127);
+            this.panel2.TabIndex = 7;
+            // 
+            // iconPictureBox2
+            // 
+            this.iconPictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.iconPictureBox2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox2.IconChar = FontAwesome.Sharp.IconChar.Key;
+            this.iconPictureBox2.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox2.IconSize = 37;
+            this.iconPictureBox2.Location = new System.Drawing.Point(3, 75);
+            this.iconPictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.iconPictureBox2.Name = "iconPictureBox2";
+            this.iconPictureBox2.Size = new System.Drawing.Size(45, 37);
+            this.iconPictureBox2.TabIndex = 7;
+            this.iconPictureBox2.TabStop = false;
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.User;
+            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
+            this.iconPictureBox1.IconSize = 37;
+            this.iconPictureBox1.Location = new System.Drawing.Point(3, 17);
+            this.iconPictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(45, 37);
+            this.iconPictureBox1.TabIndex = 6;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // txt_PassWord
+            // 
+            this.txt_PassWord.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.txt_PassWord.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txt_PassWord.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_PassWord.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txt_PassWord.HintForeColor = System.Drawing.Color.Empty;
+            this.txt_PassWord.HintText = "password";
+            this.txt_PassWord.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txt_PassWord.isPassword = false;
+            this.txt_PassWord.LineFocusedColor = System.Drawing.Color.Blue;
+            this.txt_PassWord.LineIdleColor = System.Drawing.Color.Gray;
+            this.txt_PassWord.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.txt_PassWord.LineThickness = 2;
+            this.txt_PassWord.Location = new System.Drawing.Point(48, 75);
+            this.txt_PassWord.Margin = new System.Windows.Forms.Padding(5);
+            this.txt_PassWord.Name = "txt_PassWord";
+            this.txt_PassWord.Size = new System.Drawing.Size(283, 37);
+            this.txt_PassWord.TabIndex = 5;
+            this.txt_PassWord.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_PassWord.OnValueChanged += new System.EventHandler(this.txt_PassWord_OnValueChanged);
+            this.txt_PassWord.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_PassWord_KeyPress);
+            // 
+            // lb_infor
+            // 
+            this.lb_infor.AutoSize = true;
+            this.lb_infor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_infor.ForeColor = System.Drawing.Color.DarkRed;
+            this.lb_infor.Location = new System.Drawing.Point(74, 338);
+            this.lb_infor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_infor.Name = "lb_infor";
+            this.lb_infor.Size = new System.Drawing.Size(246, 18);
+            this.lb_infor.TabIndex = 8;
+            this.lb_infor.Text = "Invalid User Name or Password";
+            this.lb_infor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lb_infor.Visible = false;
+            // 
+            // tmr_login_flase
+            // 
+            this.tmr_login_flase.Interval = 5000;
+            this.tmr_login_flase.Tick += new System.EventHandler(this.tmr_login_flase_Tick);
+            // 
+            // F_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 497);
+            this.Controls.Add(this.lb_infor);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "F_login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txt_UserName;
+        private FontAwesome.Sharp.IconButton btn_login;
+        private System.Windows.Forms.Panel panel2;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txt_PassWord;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
+        private FontAwesome.Sharp.IconButton btn_exit;
+        private System.Windows.Forms.Label lb_infor;
+        private System.Windows.Forms.Timer tmr_login_flase;
+    }
+}
+
---
+++ SW/PC_App/Hau/C#_LOGIN/C_sharp_LOGIN/C_sharp_LOGIN/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace C_sharp_LOGIN
+{   
+    public partial class F_login : Form
+    {
+        public string username = "indruino";
+        public string password = "indruino";
+        public F_login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            login_event();
+        }
+
+        private void login_event()
+        {
+            if (check_login())
+            {
+                true_login();
+            }
+            else
+            {
+                false_login();
+            }
+        }
+        private bool check_login()
+        {
+            if((this.txt_PassWord.Text == password) &&(this.txt_UserName.Text == username))
+            {
+                return true;
+            }
+            return false;
+        }
+        private void true_login()
+        {
+            F_Main frm = new F_Main();
+            this.Hide();
+            frm.Show();
+        }
+        private void false_login()
+        {
+            this.lb_infor.Visible = true;
+            tmr_login_flase.Start();
+        }
+
+        private void tmr_login_flase_Tick(object sender, EventArgs e)
+        {
+            this.lb_infor.Visible = false;
+        }
+
+        private void txt_PassWord_OnValueChanged(object sender, EventArgs e)
+        {
+            if(this.txt_PassWord.Text == null)
+            {
+                this.txt_PassWord.isPassword = false;
+            }
+            else
+            {
+                this.txt_PassWord.isPassword = true;
+            }
+        }
+
+        private void txt_PassWord_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                login_event();
+            }
+        }
+
+        private void txt_UserName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                login_event();
+            }
+        }
+    }
+}
