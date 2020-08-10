13e267c6e3c0f690369a258a95528ba336140ee5
Thanh <tanthanhvo830@gmail.com>
2020-08-10 22:58:37 +0700

    updata

---
+++ SW/Server_App/TUAN_2/Code/udp.ino
@@ -0,0 +1,52 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+/* WiFi network name and password */
+const char* ssid = "Toi Yeu Viet Nam";
+const char* password = "khongnhopass";
+
+const char * udpAddress = "192.168.43.77";
+const int udpPort = 1234;
+
+//create UDP instance
+WiFiUDP udp;
+
+void setup(){
+  Serial.begin(115200);
+  
+  //Connect to the WiFi network
+   WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  //This initializes udp and transfer buffer
+  udp.begin(udpPort);
+}
+
+void loop(){
+  //data will be sent to server
+  uint8_t buffer[50] = "hello world";
+  //send hello world to server
+  udp.beginPacket(udpAddress, udpPort);
+  udp.write(buffer, 11);
+  udp.endPacket();
+  memset(buffer, 0, 50);
+  //processing incoming packet, must be called before reading the buffer
+  udp.parsePacket();
+  //receive response from server, it will be HELLO WORLD
+  if(udp.read(buffer, 50) > 0){
+    Serial.print("Server to client: ");
+    Serial.println((char *)buffer);
+  }
+  //Wait for 1 second
+  delay(1000);
+}
