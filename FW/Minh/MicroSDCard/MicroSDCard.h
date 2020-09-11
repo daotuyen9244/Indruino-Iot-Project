ff66b40e49398ed996c869c46433d7d528cbf71e
GreatMing221998 <minh.hoang.nguyen.221998@gmail.com>
2020-09-11 20:32:32 +0700

    SDCard

---
+++ FW/Minh/MicroSDCard/MicroSDCard.h
@@ -0,0 +1,34 @@
+#ifndef _MicroSDCard_H
+#define _MicroSDCard_H
+
+#include "Arduino.h"
+#include "SD.h"
+#include "FS.h"
+#include "SPI.h"
+
+class MicroSDCard{
+    private: 
+        void _listDir(fs::FS &fs, const char * dirname, uint8_t levels);
+        void _createDir(fs::FS &fs, const char * path);
+        void _removeDir(fs::FS &fs, const char * path);
+        void _readFile(fs::FS &fs, const char * path);
+        void _writeFile(fs::FS &fs, const char * path, const char * message);
+        void _appendFile(fs::FS &fs, const char * path, const char * message);
+        void _renameFile(fs::FS &fs, const char * path1, const char * path2);
+        void _deleteFile(fs::FS &fs, const char * path);
+        void _testFileIO(fs::FS &fs, const char * path);
+    public: 
+        void listDir(const char * dirname, uint8_t levels);
+        void createDir(const char * path);
+        void removeDir(const char * path);
+        void readFile(const char * path);
+        void writeFile(const char * path, const char * message);
+        void appendFile(const char * path, const char * message);
+        void renameFile(const char * path1, const char * path2);
+        void deleteFile(const char * path);
+        void testFileIO(const char * path);
+        uint64_t SDSize();
+        boolean SDbegin();
+};
+extern MicroSDCard SDCard;
+#endif//
