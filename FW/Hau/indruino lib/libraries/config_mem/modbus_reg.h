0109ba4f391f46ed2088693a24315ad8a1a41e9a
truonghau8 <truonghau8@gmail.com>
2020-09-18 17:51:02 +0700

    update indruino lib addr, excel

--- FW/Hau/indruino lib/libraries/config_mem/modbus_reg.h
+++ FW/Hau/indruino lib/libraries/config_mem/modbus_reg.h
@@ -1,4 +1,4 @@
-//sys 8in 8 out
+//sys 8in 8 out //size 69
 
 #ifndef _MODBUS_REG_H
 #define _MODBUS_REG_H
@@ -12,30 +12,30 @@
 #define Ram_Baud1 6
 #define Ram_IdBa1 7
 #define Ram_RtDlay 8
-#define Ram_Baud2 10
-#define Ram_IdBa2 11
-#define Ram_ADC0 12
-#define Ram_ADC1 14
-#define Ram_ADC2 16
-#define Ram_ADC3 18
-#define Ram_ADC4 20
-#define Ram_ADC5 22
-#define Ram_ADC6 24
-#define Ram_ADC7 26
-#define Ram_IN_P 28
-#define Ram_OUT_P 29
-#define Ram_m_tm 30
-#define Ram_yr 31
-#define Ram_mon 33
-#define Ram_day 34
-#define Ram_dow 35
-#define Ram_hr 36
-#define Ram_min 37
-#define Ram_sec 38
-#define Ram_FN 39
-#define Ram_ex_data_time 40
+#define Ram_Baud2 9
+#define Ram_IdBa2 10
+#define Ram_ADC0 11
+#define Ram_ADC1 13
+#define Ram_ADC2 15
+#define Ram_ADC3 17
+#define Ram_ADC4 19
+#define Ram_ADC5 21
+#define Ram_ADC6 23
+#define Ram_ADC7 25
+#define Ram_IN_P 27
+#define Ram_OUT_P 28
+#define Ram_m_tm 29
+#define Ram_yr 30
+#define Ram_mon 32
+#define Ram_day 33
+#define Ram_dow 34
+#define Ram_hr 35
+#define Ram_min 36
+#define Ram_sec 37
+#define Ram_FN 38
+#define Ram_ex_data_time 39
 
-#define ext_data 50
+#define ext_data 49
 
 
 
