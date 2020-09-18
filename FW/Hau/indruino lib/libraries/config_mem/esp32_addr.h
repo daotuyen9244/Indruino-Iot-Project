73dd85fa31f03a0af4d64983554303f8c021223d
unknown <phuchoanggthd@gmail.com>
2020-09-18 14:00:24 +0700

    up git

--- FW/Hau/indruino lib/libraries/config_mem/esp32_addr.h
+++ FW/Hau/indruino lib/libraries/config_mem/esp32_addr.h
@@ -1,12 +1,45 @@
 #ifndef _ESP32_ADDR_H
 #define _ESP32_ADDR_H
 
-#define IdBr
-#define mNum
-#define FW
-#define Baud
-
-
-
+#define IdBr 0
+#define mNum 2
+#define FW 3
+#define Baud1 4
+#define IdBa1 5
+#define RtDlay 6
+#define mWiFi 7
+#define mWifi_IP 8
+#define WiFi_IP 9
+#define WiFi_Gw 13
+#define WiFi_Sub 17
+#define WiFI_User 21
+#define WiFi_Mac 41
+#define WiFi_Pass 45
+#define mPrtcol 65
+#define mqtt_server 66
+#define mqtt_user 70
+#define mqtt_pass 90
+#define mqtt_qos 110
+#define mqtt_port 111
+#define mqtt_topic_lwt 113
+#define mqtt_payload_lwt 133
+#define mqtt_return_lwt 134
+#define udp_ip 135
+#define udp_port 139
+#define tcp_ip 141
+#define tcp_port 145
+#define modTCP_IP 147
+#define modTCP_port 151
+#define ftp_ip 153
+#define ftp_user 157
+#define ftp_pass 177
+#define ftp_port 197
+#define dt_log_mode 201
+#define dt_log_type 202
+#define sv_storage_mod 203
+#define sv_storage_IP 204
+#define sv_storage_user 208
+#define sv_pass_pass 212
+#define ext_data 0
 
 #endif //_ESP32_ADDR_H
